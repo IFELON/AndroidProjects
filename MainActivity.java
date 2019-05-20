fb7d8800fc07000ae7eb1ad1583373595430f03c
IFELON <babu.suri94@gmail.com>
2019-05-20 06:48:38 +0530

    NumberShapes
    This App is completely about numbers and their shapes i have developed this app in two possible ways it will tell weather the number is Triangular or Square .I hope this will useful for you guys

--- MainActivity.java
+++ MainActivity.java
@@ -1,45 +1,85 @@
-package in.suriya.higherorlower;
+package in.suriya.numbershapes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
 
-    int randomNumber;
-    public void makeToast(String string){
-        Toast.makeText(this,string, Toast.LENGTH_SHORT).show();
-    }
+    class Number{
 
-    public void guess(View view){
+        int number;
 
-        EditText guessEdittext = (EditText)findViewById(R.id.guessEdttxt);
-        int guessint = Integer.parseInt(guessEdittext.getText().toString());
-        
-        if (guessint>randomNumber){
-            makeToast("Lower");
-        }else if (guessint<randomNumber){
-            makeToast("Higher");
-        }else{
-            makeToast("That's right! Try Again!");
+        public boolean isSquare(){
+            double squareRoot = Math.sqrt(number);
+            if(squareRoot == Math.floor(squareRoot)){
 
-            Random rand = new Random();
-            randomNumber = rand.nextInt(20)+1;
+                return true;
+
+            }else{
+                return false;
+            }
+
         }
 
+        public boolean isTriangular(){
+
+            int x = 1;
+
+            int triangularNumber = 1;
+
+            while(triangularNumber<number){
+
+                x++;
+
+                triangularNumber =triangularNumber+x;
+            }
+            if(triangularNumber == number){
+
+                return true;
+
+            }else{
+                return false;
+            }
+        }
     }
 
+    public void testNumber(View view) {
+        EditText usersNumber = (EditText) findViewById(R.id.UsersNumber);
+
+        String message = "";
+
+        if (usersNumber.getText().toString().isEmpty()) {
+            message = "Please enter a number";
+        } else {
+
+            Log.i("UsersNumber", usersNumber.getText().toString());
+
+            Number myNumber = new Number();
+            myNumber.number = Integer.parseInt(usersNumber.getText().toString());
+
+            if (myNumber.isSquare()) {
+                if (myNumber.isTriangular()) {
+                    message = myNumber.number + "IS both Triangular & Square";
+                } else {
+                    message = myNumber.number + "Is Square but not Triangular.";
+                }
+            } else {
+                if (myNumber.isTriangular()) {
+                    message = myNumber.number + "IS Triangular but not Square";
+                } else {
+                    message = myNumber.number + "Neither Square  nor Triangular.";
+                }
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Random rand = new Random();
-        randomNumber = rand.nextInt(20)+1;
-
     }
 }
