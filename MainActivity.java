97d2bdee353c1ed4ca207dca9a37dcf12a07dbc5
IFELON <babu.suri94@gmail.com>
2019-05-25 10:55:22 +0530

    Add files via upload
    Animations (Different types of effects).

--- MainActivity.java
+++ MainActivity.java
@@ -1,85 +1,33 @@
-package in.suriya.numbershapes;
+package in.suriya.animations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    class Number{
+    public void fading(View view){
 
-        int number;
-
-        public boolean isSquare(){
-            double squareRoot = Math.sqrt(number);
-            if(squareRoot == Math.floor(squareRoot)){
-
-                return true;
-
-            }else{
-                return false;
-            }
-
-        }
-
-        public boolean isTriangular(){
-
-            int x = 1;
-
-            int triangularNumber = 1;
-
-            while(triangularNumber<number){
-
-                x++;
-
-                triangularNumber =triangularNumber+x;
-            }
-            if(triangularNumber == number){
-
-                return true;
-
-            }else{
-                return false;
-            }
-        }
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+        bart.animate()
+                .translationXBy(1000)
+                .translationYBy(1000)
+                .rotation(3600)
+                .setDuration(2000);
+/*
+        ImageView img2 = (ImageView)findViewById(R.id.img2);
+        img2.animate().alphaBy(1).setDuration(2000);*/
     }
 
-    public void testNumber(View view) {
-        EditText usersNumber = (EditText) findViewById(R.id.UsersNumber);
-
-        String message = "";
-
-        if (usersNumber.getText().toString().isEmpty()) {
-            message = "Please enter a number";
-        } else {
-
-            Log.i("UsersNumber", usersNumber.getText().toString());
-
-            Number myNumber = new Number();
-            myNumber.number = Integer.parseInt(usersNumber.getText().toString());
-
-            if (myNumber.isSquare()) {
-                if (myNumber.isTriangular()) {
-                    message = myNumber.number + "IS both Triangular & Square";
-                } else {
-                    message = myNumber.number + "Is Square but not Triangular.";
-                }
-            } else {
-                if (myNumber.isTriangular()) {
-                    message = myNumber.number + "IS Triangular but not Square";
-                } else {
-                    message = myNumber.number + "Neither Square  nor Triangular.";
-                }
-            }
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView bart = (ImageView) findViewById(R.id.bart);
+        bart.setTranslationX(-1000);
+        bart.setTranslationY(-1000);
+
     }
 }
